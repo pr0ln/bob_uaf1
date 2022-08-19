125cba90424eb3172a37880c3916d268bc4dbbb4
Gwangun Jung <exsociety@gmail.com>
2022-08-19 23:06:52 +0900

    final

--- bob_uaf.c
+++ bob_uaf.c
@@ -166,8 +166,6 @@ static long device_ioctl(struct file *file, unsigned int cmd, unsigned long args
 
 			return vuln_write64(&input);
 		case IOCTL_VULN:
-			if (copy_from_user(&input, (struct vuln_input __user *)args, sizeof(input)))
-                                return -EFAULT;
                         return vuln_vulnfunc((unsigned int)args);
 		default:
 			break;
